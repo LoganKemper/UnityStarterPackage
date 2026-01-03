6ec51de34145e561d35da3649560c5fe7154d7fa
Logan Kemper <LoganKemper@users.noreply.github.com>
2026-01-03 16:47:43 -0500

    Added rigidbody movement option to PatrolSimple3D

--- 3D Starter Package/3D_Starter_Package_Scripts_V2/PatrolSimple3D.cs
+++ 3D Starter Package/3D_Starter_Package_Scripts_V2/PatrolSimple3D.cs
@@ -11,6 +11,12 @@ namespace DigitalWorlds.StarterPackage3D
     /// </summary>
     public class PatrolSimple3D : MonoBehaviour
     {
+        private enum MovementType : byte
+        {
+            Translation,
+            Rigidbody
+        }
+
         [Tooltip("The other position that this GameObject should move to.")]
         [SerializeField] private Transform pointB;
 
@@ -20,23 +26,42 @@ namespace DigitalWorlds.StarterPackage3D
         [Tooltip("How close this GameObject needs to be from the end points to switch target. May need to be adjusted depending on the scale of your game.")]
         [SerializeField] private float distanceThreshold = 0.05f;
 
+        [Tooltip("Choose whether to translate the position of this object, or affect its rigidbody. Rigidbody movement will be the better option for physics collisions.")]
+        [SerializeField] private MovementType movementType = MovementType.Translation;
+
+        private Rigidbody rb;
         private Vector3 pointAPosition;
-        private bool isRight = true;
+        private bool toB = true;
 
         private void Start()
         {
+            // If using rigidbody movement, get the rigidbody component and set it up
+            if (movementType == MovementType.Rigidbody)
+            {
+                rb = GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+                rb.interpolation = RigidbodyInterpolation.Interpolate;
+            }
+
+            // Save the initial position as point A
             pointAPosition = transform.position;
         }
 
+        // Perform translation in Update
         private void Update()
         {
-            if (isRight)
+            if (movementType != MovementType.Translation)
+            {
+                return;
+            }
+
+            if (toB)
             {
                 transform.position = Vector3.MoveTowards(transform.position, pointB.position, speed * Time.deltaTime);
 
                 if (Vector3.Distance(transform.position, pointB.position) < distanceThreshold)
                 {
-                    isRight = false;
+                    toB = false;
                 }
             }
             else
@@ -45,7 +70,37 @@ namespace DigitalWorlds.StarterPackage3D
 
                 if (Vector3.Distance(transform.position, pointAPosition) < distanceThreshold)
                 {
-                    isRight = true;
+                    toB = true;
+                }
+            }
+        }
+
+        // Perform rigidbody movement in FixedUpdate
+        private void FixedUpdate()
+        {
+            if (movementType != MovementType.Rigidbody || rb == null)
+            {
+                return;
+            }
+
+            if (toB)
+            {
+                Vector3 next = Vector3.MoveTowards(rb.position, pointB.position, speed * Time.fixedDeltaTime);
+                rb.MovePosition(next);
+
+                if (Vector3.Distance(rb.position, pointB.position) < distanceThreshold)
+                {
+                    toB = false;
+                }
+            }
+            else
+            {
+                Vector3 next = Vector3.MoveTowards(rb.position, pointAPosition, speed * Time.fixedDeltaTime);
+                rb.MovePosition(next);
+
+                if (Vector3.Distance(rb.position, pointAPosition) < distanceThreshold)
+                {
+                    toB = true;
                 }
             }
         }
